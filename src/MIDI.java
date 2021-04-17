d17750f423dac9234f4b72610343f6d48ca1177c
Crochar6 <roch.carles@gmail.com>
2021-04-17 20:03:31 +0200

    DPOO2021C8-34 Created SongNote and started reading process

--- src/MIDI.java
+++ src/MIDI.java
@@ -1,6 +1,7 @@
 import javax.sound.midi.*;
 import java.io.File;
 import java.lang.String;
+import java.util.ArrayList;
 
 public class MIDI {
     public static final int NOTE_ON = 0x90;
@@ -9,34 +10,37 @@ public class MIDI {
 
     public static void main(String[] args) throws Exception {
         Sequence sequence = MidiSystem.getSequence(new File("test.mid"));
-
+        ArrayList<SongNote> notes = new ArrayList<>();
+        
         int trackNumber = 0;
         for (Track track :  sequence.getTracks()) {
             trackNumber++;
             System.out.println("Track " + trackNumber + ": size = " + track.size());
             System.out.println();
+
             for (int i=0; i < track.size(); i++) {
                 MidiEvent event = track.get(i);
-                System.out.print("@" + event.getTick() + " ");
+                //System.out.print("@" + event.getTick() + " ");
+
                 MidiMessage message = event.getMessage();
+
                 if (message instanceof ShortMessage) {
                     ShortMessage sm = (ShortMessage) message;
-                    System.out.print("Channel: " + sm.getChannel() + " ");
-                    if (sm.getCommand() == NOTE_ON) {
+                    //System.out.print("Channel: " + sm.getChannel() + " ");
+
+                    // If the message is a note press / release
+                    if (sm.getCommand() == NOTE_ON || sm.getCommand() == NOTE_OFF) {
+                        long tick = event.getTick();
                         int key = sm.getData1();
-                        int octave = (key / 12)-1;
+                        int octave = Math.min(Math.max((key / 12)-1, 1), 2);    // octave must be either 1 or 2
                         int note = key % 12;
                         String noteName = NOTE_NAMES[note];
-                        int velocity = sm.getData2();
-                        System.out.println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity);
-                    } else if (sm.getCommand() == NOTE_OFF) {
-                        int key = sm.getData1();
-                        int octave = (key / 12)-1;
-                        int note = key % 12;
-                        String noteName = NOTE_NAMES[note];
-                        int velocity = sm.getData2();
-                        System.out.println("Note off, " + noteName + octave + " key=" + key + " velocity: " + velocity);
-                    } else {
+                        byte velocity = (byte)sm.getData2();
+                        //TODO: Add song to the list
+                        //notes.add(new SongNote(tick, (sm.getCommand() == NOTE_ON), velocity, note + Note.Do1 + (12*octave)));
+                        //System.out.println("Note off, " + noteName + octave + " key=" + key + " velocity: " + velocity);
+                    }
+                    else {
                         System.out.println("Command:" + sm.getCommand());
                     }
                 } else {
