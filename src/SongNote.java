e1f57c33c366ffced3b4a2a39844467e239242fe
Carles Roch <carles.roch@students.salle.url.edu>
2021-04-17 20:03:31 +0200

    DPOO2021C8-34 Created SongNote and started reading process

--- src/SongNote.java
+++ src/SongNote.java
@@ -1,3 +1,13 @@
 public class SongNote {
+    private long tick;
+    private boolean pressed;
+    private byte velocity;
+    private Note note;
 
+    public SongNote(long tick, boolean pressed, byte velocity, Note note) {
+        this.tick = tick;
+        this.pressed = pressed;
+        this.velocity = velocity;
+        this.note = note;
+    }
 }
