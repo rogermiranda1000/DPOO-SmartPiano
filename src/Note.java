51176b0245b5eb6270e2934c024a1058c5fae400
roger.miranda <roger.miranda@students.salle.url.edu>
2021-04-20 13:55:37 +0200

    MIDI octaves

--- src/Note.java
+++ src/Note.java
@@ -1,32 +1,20 @@
 public enum Note {
-    Do1,
-    DoX1,
-    Re1,
-    ReX1,
-    Mi1,
-    Fa1,
-    FaX1,
-    Sol1,
-    SolX1,
-    La1,
-    LaX1,
-    Si1,
-    Do2,
-    DoX2,
-    Re2,
-    ReX2,
-    Mi2,
-    Fa2,
-    FaX2,
-    Sol2,
-    SolX2,
-    La2,
-    LaX2,
-    Si2;
+    Do,
+    DoX,
+    Re,
+    ReX,
+    Mi,
+    Fa,
+    FaX,
+    Sol,
+    SolX,
+    La,
+    LaX,
+    Si;
 
     public static Note getNote(int index) {
         Note[] r = Note.values();
-        index %= r.length; // TODO que fem???
+        index %= r.length;
         return r[index];
     }
 }
