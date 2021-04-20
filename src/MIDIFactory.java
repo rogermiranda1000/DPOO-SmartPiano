67df1c0bc39aa55d8fb2590e43f4ee4cac59f2c0
roger.miranda <roger.miranda@students.salle.url.edu>
2021-04-20 22:35:11 +0200

    MIDI temps per tick

--- src/MIDIFactory.java
+++ src/MIDIFactory.java
@@ -1,19 +1,29 @@
 import javax.sound.midi.*;
 import java.io.File;
+import java.io.IOException;
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 
-public class MIDI {
+public class MIDIFactory {
     public static final int NOTE_ON = 0x90;
     public static final int NOTE_OFF = 0x80;
 
-    public static void main(String[] args) throws Exception {
-        Sequence sequence = MidiSystem.getSequence(new File("test.mid"));
+    /**
+     * Donat un fitxer .mid extreu les tecles
+     * @param path Ruta del fitxer
+     * @return Conço (array de notes)
+     * @throws IOException Error al obrir el fitxer
+     * @throws InvalidMidiDataException Error al tractar el fitxer com MIDI
+     */
+    public static SongNote[] getSong(File path) throws IOException, InvalidMidiDataException {
         ArrayList<SongNote> notes = new ArrayList<>();
 
-        for (Track track :  sequence.getTracks()) {
+        Sequence sequence = MidiSystem.getSequence(path);
+
+        double tickLenght = (double) sequence.getMicrosecondLength()/sequence.getTickLength(); // temps [us] per tick
+
+        for (Track track : sequence.getTracks()) {
             //System.out.println("Track " + trackNumber + ": size = " + track.size());
 
             for (int i=0; i < track.size(); i++) {
@@ -39,6 +49,15 @@ public class MIDI {
         }
 
         Collections.sort(notes);
+        return notes.toArray(new SongNote[0]);
+    }
+
+    public static void generateSong(SongNote[] notes) {
+        // TODO
+    }
+
+    public static void main(String[] args) throws Exception {
+        MIDIFactory.getSong(new File("test.mid"));
         System.out.println();
 
     }
