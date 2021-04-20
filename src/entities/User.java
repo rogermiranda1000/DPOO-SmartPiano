c1da4e6917a7aee6d0d68c38193d2c08bf97b7af
roger.miranda <roger.miranda@students.salle.url.edu>
2021-04-20 11:44:21 +0200

    DPOO2021C8-12 insert

--- src/entities/User.java
+++ src/entities/User.java
@@ -2,6 +2,19 @@ package entities;
 
 public class User {
     private String name;
-    private String mail;
+    private String email;
     // TODO SongList
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
 }
