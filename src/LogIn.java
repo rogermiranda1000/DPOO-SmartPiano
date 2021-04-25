edea917d8922d6b2dc3770c9616a094aa12a618e
Pau <ppassolas@gmail.com>
2021-04-25 17:37:09 +0200

    Layers implementation

--- src/LogIn.java
+++ src/LogIn.java
@@ -14,15 +14,13 @@ public class LogIn extends JDialog implements ActionListener {
     private static final int HEIGHT = 400;
 
     private JButton loginButton, registerButton, backButton, pushResgisterButton;
-    private TextField usernameInput;
-    private TextField emailInput;
-    private JPasswordField passwordInput;
+    private TextField usernameInput, emailRegister, usernameRegister;
+    private JPasswordField passwordInput, passwordRegister;
     private LoginEvent event;
-    JLayeredPane mainContent = new JLayeredPane();
+    JPanel mainContent;
+    CardLayout cl;
 
-    private ArrayList<JPanel> layers = new ArrayList<JPanel>();
-
-    public LogIn(LoginEvent event, Frame frame){
+    public LogIn(LoginEvent event, Frame frame) {
         super(frame, true);
         this.event = event;
 
@@ -32,15 +30,17 @@ public class LogIn extends JDialog implements ActionListener {
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        mainContent.setBounds(0,0,400,400);
-        mainContent.add(buildLogin(), Integer.valueOf(0));
-        mainContent.add(buildRegister(), Integer.valueOf(1));
+        mainContent = new JPanel(new CardLayout());
+        mainContent.add(buildLogin(), "login");
+        mainContent.add(buildRegister(), "register");
         this.add(mainContent);
-        mainContent.moveToFront(mainContent.getComponent(0));
+        cl = (CardLayout) (mainContent.getLayout());
+        cl.show(mainContent, ("login"));
+
         this.setVisible(true);
     }
 
-    public JPanel buildLogin(){
+    public JPanel buildLogin() {
         JPanel container = new JPanel(new BorderLayout());
         container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
 
@@ -48,7 +48,7 @@ public class LogIn extends JDialog implements ActionListener {
         JPanel logo = new JPanel();
         logo.setBackground(ColorConstants.BACKGROUND.getColor());
         logo.setVisible(true);
-        logo.setMaximumSize(new Dimension(WIDTH,200));
+        logo.setMaximumSize(new Dimension(WIDTH, 200));
         String LOGO_PATH = "images\\smartpiano.png";
 
         try {
@@ -64,10 +64,10 @@ public class LogIn extends JDialog implements ActionListener {
         JPanel username = new JPanel();
         username.setBackground(ColorConstants.BACKGROUND.getColor());
         username.setVisible(true);
-        username.setMaximumSize(new Dimension(WIDTH,45));
+        username.setMaximumSize(new Dimension(WIDTH, 45));
 
         Label usernameTxt = new Label("Username :");
-        usernameTxt.setPreferredSize(new Dimension(75,20));
+        usernameTxt.setPreferredSize(new Dimension(75, 20));
 
         usernameInput = new TextField();
         usernameInput.setPreferredSize(new Dimension(180, 20));
@@ -79,12 +79,12 @@ public class LogIn extends JDialog implements ActionListener {
         JPanel password = new JPanel();
         password.setBackground(ColorConstants.BACKGROUND.getColor());
         password.setVisible(true);
-        password.setMaximumSize(new Dimension(WIDTH,45));
+        password.setMaximumSize(new Dimension(WIDTH, 45));
 
         Label passwordTxt = new Label("Password :");
-        passwordTxt.setPreferredSize(new Dimension(75,20));
+        passwordTxt.setPreferredSize(new Dimension(75, 20));
 
-        passwordInput=new JPasswordField();
+        passwordInput = new JPasswordField();
         passwordInput.setPreferredSize(new Dimension(180, 20));
 
         password.add(passwordTxt);
@@ -94,7 +94,7 @@ public class LogIn extends JDialog implements ActionListener {
         JPanel buttons = new JPanel();
         buttons.setBackground(ColorConstants.BACKGROUND.getColor());
         buttons.setVisible(true);
-        buttons.setMaximumSize(new Dimension(WIDTH,100));
+        buttons.setMaximumSize(new Dimension(WIDTH, 100));
 
         loginButton = new JButton("Sign In");
         registerButton = new JButton("Register");
@@ -119,60 +119,76 @@ public class LogIn extends JDialog implements ActionListener {
         return container;
     }
 
-    public JPanel buildRegister(){
+    public JPanel buildRegister() {
         JPanel container = new JPanel(new BorderLayout());
         container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+
+        // Header
+        JPanel header = new JPanel();
+        header.setBackground(ColorConstants.BACKGROUND.getColor());
+        header.setVisible(true);
+        header.setMaximumSize(new Dimension(WIDTH, 200));
+        String IMG_PATH = "images\\register.png";
+
+        try {
+            BufferedImage img = ImageIO.read(new File(IMG_PATH));
+            ImageIcon icon = new ImageIcon(img);
+            JLabel logoLabel = new JLabel(icon);
+            header.add(logoLabel, BorderLayout.CENTER);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Username
         JPanel username = new JPanel();
         username.setBackground(ColorConstants.BACKGROUND.getColor());
         username.setVisible(true);
-        username.setMaximumSize(new Dimension(WIDTH,45));
+        username.setMaximumSize(new Dimension(WIDTH, 45));
 
         Label usernameTxt = new Label("Username :");
-        usernameTxt.setPreferredSize(new Dimension(75,20));
+        usernameTxt.setPreferredSize(new Dimension(75, 20));
 
-        usernameInput = new TextField();
-        usernameInput.setPreferredSize(new Dimension(180, 20));
+        usernameRegister = new TextField();
+        usernameRegister.setPreferredSize(new Dimension(180, 20));
 
         username.add(usernameTxt);
-        username.add(usernameInput);
+        username.add(usernameRegister);
 
         // Email
         JPanel email = new JPanel();
         email.setBackground(ColorConstants.BACKGROUND.getColor());
         email.setVisible(true);
-        email.setMaximumSize(new Dimension(WIDTH,45));
+        email.setMaximumSize(new Dimension(WIDTH, 45));
 
         Label emailTxt = new Label("Email :");
-        emailTxt.setPreferredSize(new Dimension(75,20));
+        emailTxt.setPreferredSize(new Dimension(75, 20));
 
-        emailInput = new TextField();
-        emailInput.setPreferredSize(new Dimension(180, 20));
+        emailRegister = new TextField();
+        emailRegister.setPreferredSize(new Dimension(180, 20));
 
         email.add(emailTxt);
-        email.add(emailInput);
+        email.add(emailRegister);
 
         // Password
         JPanel password = new JPanel();
         password.setBackground(ColorConstants.BACKGROUND.getColor());
         password.setVisible(true);
-        password.setMaximumSize(new Dimension(WIDTH,45));
+        password.setMaximumSize(new Dimension(WIDTH, 45));
 
         Label passwordTxt = new Label("Password :");
-        passwordTxt.setPreferredSize(new Dimension(75,20));
+        passwordTxt.setPreferredSize(new Dimension(75, 20));
 
-        passwordInput=new JPasswordField();
-        passwordInput.setPreferredSize(new Dimension(180, 20));
+        passwordRegister = new JPasswordField();
+        passwordRegister.setPreferredSize(new Dimension(180, 20));
 
         password.add(passwordTxt);
-        password.add(passwordInput);
+        password.add(passwordRegister);
 
         // Buttons
         JPanel buttons = new JPanel();
         buttons.setBackground(ColorConstants.BACKGROUND.getColor());
         buttons.setVisible(true);
-        buttons.setMaximumSize(new Dimension(WIDTH,100));
+        buttons.setMaximumSize(new Dimension(WIDTH, 100));
 
         backButton = new JButton("Back to LogIn");
         pushResgisterButton = new JButton("Register");
@@ -181,6 +197,7 @@ public class LogIn extends JDialog implements ActionListener {
         buttons.add(pushResgisterButton, BorderLayout.CENTER);
 
         // Add of all the components
+        container.add(header, BorderLayout.CENTER);
         container.add(username, BorderLayout.CENTER);
         container.add(password, BorderLayout.CENTER);
         container.add(email, BorderLayout.CENTER);
@@ -197,16 +214,17 @@ public class LogIn extends JDialog implements ActionListener {
         return container;
     }
 
-    private void wrongPass(){
+    private void wrongPass() {
         JOptionPane.showMessageDialog(this, "Wrong combination. Try it again ( ͡❛ ︹ ͡❛)");
     }
 
-    private void userCreated(){
+    private void userCreated() {
         JOptionPane.showMessageDialog(this, "User created successfully ¯\\_( ͡❛ ‿‿ ͡❛)_/¯");
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == loginButton) {
+        if (e.getSource() == loginButton) {
             if (event.requestLogin(usernameInput.getText(), String.valueOf(passwordInput.getPassword()))) {
                 this.dispose();
             } else {
@@ -214,24 +232,22 @@ public class LogIn extends JDialog implements ActionListener {
                 usernameInput.setText("");
                 passwordInput.setText("");
             }
-
         } else if (e.getSource() == registerButton) {
-            mainContent.getComponentCountInLayer(1);
+            cl.show(mainContent, ("register"));
         } else if (e.getSource() == backButton) {
-            mainContent.getComponentCountInLayer(0);
+            cl.show(mainContent, ("login"));
         } else if (e.getSource() == pushResgisterButton) {
-            if (event.requestRegister(usernameInput.getText(), emailInput.getText(), String.valueOf(passwordInput.getPassword()))) {
+            if (event.requestRegister(usernameRegister.getText(), emailRegister.getText(), String.valueOf(passwordRegister.getPassword()))) {
                 userCreated();
-                usernameInput.setText("");
-                emailInput.setText("");
-                passwordInput.setText("");
-                this.setContentPane(layers.get(0));
-                this.repaint();
+                usernameRegister.setText("");
+                emailRegister.setText("");
+                passwordRegister.setText("");
+                cl.show(mainContent, ("login"));
             } else {
                 wrongPass();
-                usernameInput.setText("");
-                passwordInput.setText("");
-                emailInput.setText("");
+                usernameRegister.setText("");
+                passwordRegister.setText("");
+                emailRegister.setText("");
             }
         }
     }
