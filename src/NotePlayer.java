d4a97cb0469adc40e8772c1d3710e2c60325f0cb
Carles Roch <carles.roch@students.salle.url.edu>
2021-05-04 13:17:31 +0200

    DPOO2021C8-34 Play single note

--- src/NotePlayer.java
+++ src/NotePlayer.java
@@ -1,4 +1,5 @@
-package entities;
+import entities.Song;
+import entities.SongNote;
 
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
@@ -7,6 +8,7 @@ import javax.sound.midi.Synthesizer;
 import java.util.ArrayList;
 
 public class NotePlayer implements Runnable {
+    private static NotePlayer globalNotePlayer = new NotePlayer();
     private static final int INSTRUMENT = 0;
     private static final int SPEED = 1;
 
@@ -15,13 +17,28 @@ public class NotePlayer implements Runnable {
     private final MidiChannel[] channels;
     private final Synthesizer synth;
 
-    public NotePlayer(Song song) throws MidiUnavailableException {
+
+    public NotePlayer(Song song) {
         this.notes = song.getNotes();
         this.tickLength = song.getTickLength();
+        MidiChannel[] channels = null;
+        Synthesizer synth = null;
+        try {
+            synth = MidiSystem.getSynthesizer();
+            synth.open();
+            channels = synth.getChannels();
+        } catch (MidiUnavailableException ex) {
+            ex.printStackTrace();
+        }
+        this.channels = channels;
+        this.synth = synth;
+    }
 
-        this.synth = MidiSystem.getSynthesizer();
-        this.synth.open();
-        this.channels = this.synth.getChannels();
+    /**
+     * Constructor del singleton
+     */
+    public NotePlayer() {
+        this(new Song("","",null, 1));
     }
 
     @Override
@@ -51,6 +68,10 @@ public class NotePlayer implements Runnable {
         this.synth.close();
     }
 
+    public static void closeSinglePlayer() {
+        NotePlayer.globalNotePlayer.closeSynth();
+    }
+
     public void executeNote(SongNote note) {
         if (note.isPressed()) {
             this.channels[INSTRUMENT].noteOn(note.getId(), note.getVelocity());
@@ -59,7 +80,7 @@ public class NotePlayer implements Runnable {
         }
     }
 
-    /*public static void executeNote(SongNote note, double tickLength) throws MidiUnavailableException {
-        new NotePlayer(tickLength).executeNote(note);
-    }*/
+    public static void executeSingleNote(SongNote note) {
+         NotePlayer.globalNotePlayer.executeNote(note);
+    }
 }
