c783ad1239f27fc0f45bd148ebbadd243d77bfcd
Pau <ppassolas@gmail.com>
2021-05-13 11:16:03 +0200

    Main fix

--- src/Main.java
+++ src/Main.java
@@ -20,44 +20,12 @@ public class Main {
                             "╚══╩╩═╩╩╝╚╩╩╝╚╝░╚╝╚══╩╝╚╩╩═╩═╝"); //Roger, no borris.
 
 
-        LoginEvent e = new LoginEvent() {
-            @Override
-            public boolean requestLogin(String user, String password) {
-                // TODO: Check in ddbb if user exists.
-                return user.contains("admin") && password.contains("admin");
-            }
-
-            @Override
-            public boolean requestRegister(String user, String email, String password) {
-                // TODO: Check if user already exists and no empty fields.
-                return true;
-            }
-        };
-
-        MenuEvent m = new MenuEvent() {
-            @Override
-            public boolean toggleLoop() {
-                // TODO: Check in player if loop mode on.
-                return false;
-            }
-
-            @Override
-            public int currentSongPos() {
-                // TODO: check current song position on playing list
-                return 0;
-            }
-
-            @Override
-            public boolean playing() {
-                // TODO: Check if a song is playing
-                return false;
-            }
-        };
-
-        SongsEvent s = new SongsEvent() {
-
-        };
-
-        Menu f = new Menu(e,m,s);
+        try {
+            Config c = new Config(new File("config.json"));
+            DDBBAccess ddbb = new DDBBAccess(c.readConfig());
+            new Controller(c.getScrapping(), new SongDDBBDAO(ddbb), new UserDDBBDAO(ddbb));
+        } catch (FileNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
