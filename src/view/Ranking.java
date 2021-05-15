49252d5065cee9ad801c3568187513d0b9059fb8
Conrad Romgosa <conrad.romagosa@students.salle.url.edu>
2021-05-15 22:23:47 +0200

    Implementation of the class GraphDrawer which draws a graphic with ana array of given int values. Drowing the 2 graphs in our Ranking panel.

--- src/view/Ranking.java
+++ src/view/Ranking.java
@@ -4,12 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Ranking extends JPanel {
+    int listenedSongs[];
+    int listenedTime[];
+    //TODO Importar la llista de valors per als ranqings
 
     public Ranking(){
-
-
-        this.setBackground(Color.red);
-
-        
+        this.setLayout(new BorderLayout(10, 10));
+        GraphDrawer songsGraph = new GraphDrawer(listenedSongs, "Listened Songs per hour", new Color(0xFF0000));
+        GraphDrawer timeGraph = new GraphDrawer(listenedTime, "Listened Time per hour", new Color(0x016618));
+        this.add(songsGraph, BorderLayout.WEST);
+        this.add(timeGraph, BorderLayout.EAST);
+        this.setBackground(ColorConstants.BACKGROUND.getColor());
+        this.setVisible(true);
     }
+
+
 }
