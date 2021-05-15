8a27a0f24b876f60082c75d6d0b7000f7d541909
Crochar6 <roch.carles@gmail.com>
2021-05-15 15:57:24 +0200

    Started addListen

--- src/persistance/StatisticsDAO.java
+++ src/persistance/StatisticsDAO.java
@@ -1,6 +1,9 @@
 package persistance;
 
+import entities.Song;
+
 public interface StatisticsDAO {
     public int[] getSongStatistics();
     public int[] getTimeStatistics();
+    public boolean addListen(Song song, int seconds);
 }
