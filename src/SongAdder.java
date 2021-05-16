d08fc5290668649a0a0fc78a03a3966eb48157df
roger.miranda <roger.miranda@students.salle.url.edu>
2021-05-16 22:33:40 +0200

    Added more songs :)

--- src/SongAdder.java
+++ src/SongAdder.java
@@ -10,16 +10,20 @@ import java.sql.SQLException;
 import java.util.Date;
 
 public class SongAdder {
-    private static final String NAME = "Megalovania";
-    private static final String URL = "http://www.vgmusic.com/new-files/UT-Megalovania.mid";
+    /*private static final String []SONG = {"Megalovania", "http://www.vgmusic.com/new-files/UT-Megalovania.mid"};
+    private static final String []SONG = {"L'himne del Carbassot 100% oficial 1 sol link mega", "https://www.midiworld.com/download/5023"};
+    private static final String []SONG = {"Mii Channel", "https://www.midiworld.com/download/5037"};
+    private static final String []SONG = {"L'himne d'Espanya", "https://www.midiworld.com/download/4163"};
+    private static final String []SONG = {"All star", "https://www.midiworld.com/download/854"};*/
+    private static final String []SONG = {"Windows startup", "https://www.midiworld.com/download/4806"};
 
     public static void main (String[] args) {
         try {
             Config c = new Config(new File("config.json"));
             DDBBAccess ddbb = new DDBBAccess(c.readConfig(), 1);
             try {
-                URL url = new URL(URL);
-                new SongDDBBDAO(ddbb).addVirtualSong(MIDIFactory.getSong(NAME, "admin", new Date(), url));
+                URL url = new URL(SONG[1]);
+                new SongDDBBDAO(ddbb).addVirtualSong(MIDIFactory.getSong(SONG[0], "admin", new Date(), url));
             } catch (IOException |InvalidMidiDataException e) {
                 e.printStackTrace();
             }
