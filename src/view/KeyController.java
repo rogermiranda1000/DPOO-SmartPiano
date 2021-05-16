867b65ea510b8574fddb92030821b641d5b0d86a
David Larrosa Camps <david.larrosa@students.salle.url.edu>
2021-05-16 12:54:21 +0200

    Listeners de piano i sliders fets però no funcionals, encara

--- src/view/KeyController.java
+++ src/view/KeyController.java
@@ -3,6 +3,6 @@ package view;
 public interface KeyController {
     boolean isPressed(String id);
     boolean isNotPressed(String id);
-    boolean keyBoardPressed(String id);
-    boolean keyBoardNotPressed(String id);
+    boolean keyBoardPressed(char nota);
+    boolean keyBoardNotPressed(char nota);
 }
---
+++ src/view/SliderController.java
@@ -0,0 +1,5 @@
+package view;
+
+public interface SliderController {
+    void changeValue(int value);
+}
