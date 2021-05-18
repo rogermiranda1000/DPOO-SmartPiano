c6be8b1d488cc5f2ac33a3693ac6ab78b91435aa
Conrad Romgosa <conrad.romagosa@students.salle.url.edu>
2021-05-18 19:20:16 +0200

    Correction: the graph now starts properly and not after updating the panel.

--- src/view/GraphDrawer.java
+++ src/view/GraphDrawer.java
@@ -1,30 +1,31 @@
 package view;
 
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.util.Date;
 
 
-class GraphDrawer extends JPanel implements ComponentListener {
+class GraphDrawer extends JPanel {
     private final int[] yCoords;
     private final int startX = 50;
-    private int endX = 1000;
-    private int endY = 562;
+    private final int startY = 20;
+    private int endX = this.getWidth() - 50;
+    private int endY = this.getHeight() - 160;
     private int prevX = startX;
     private int prevY = endY;
-    private int unitX = (endX - startX) / 23;
+    private int unitX;
+    private int unitY;
+    String name;
+    Color lineColor;
     Date time = new Date();
 
 
     public GraphDrawer(int[] yCoords, String name, Color lineColor) {
-        this.setSize(1000, 1000);
-        endX = this.getWidth() - 50;
-        endY = this.getHeight() - 160;
         this.yCoords = yCoords;
+        this.name = name;
+        this.lineColor = lineColor;
     }
-
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -33,12 +34,19 @@ class GraphDrawer extends JPanel implements ComponentListener {
 
         int topElementY = GetMax(yCoords);
         int hour = time.getHours();
-        int startY = 35;
-        int unitY = (endY - startY) / topElementY;
+        int numHoritzontalLines = numSections(topElementY);
+
+        endX = this.getHeight() - 50;
+        endY = this.getWidth() - 160;
+        unitX = (endX - startX) / 23;
+        unitY = (endY - startY) / numHoritzontalLines;
 
 
         g2d.setFont(new Font("TimesRoman", Font.BOLD, 14));
+
+        //Dibuix de les lineas verticals
         for (int i = startX; i <= endX; i += unitX) {
+
             g2d.drawLine(i, startY, i, endY);
             g2d.setColor(Color.BLACK);
             g2d.rotate(-0.78, i, endY + 30);
@@ -51,41 +59,34 @@ class GraphDrawer extends JPanel implements ComponentListener {
             }
         }
 
-
+        //dibuix de les lineas horitzontals
+        int count = 0;
         for (int i = endY; i > startY; i -= unitY) {
             g2d.drawLine(startX, i, endX, i);
             g2d.setColor(Color.BLACK);
-            if ((i - startY) / unitY == topElementY / 2) {
-                if (topElementY % 2 == 0) {
-                    g2d.drawString(Integer.toString(topElementY / 2), startX - 20, i + 5);
-                } else {
-                    g2d.drawString(Integer.toString(topElementY / 2 + 1), startX - 20, i + 5);
-                }
-                g2d.setColor(Color.DARK_GRAY);
-            }
+            g2d.drawString(Integer.toString(topElementY*count/numHoritzontalLines), startX - 45, i + 5);
+            g2d.setColor(Color.DARK_GRAY);
+            count++;
         }
 
 
+
         boolean firstElement = true;
-        g2d.setColor(Color.RED);
+        g2d.setColor(lineColor);
         g2d.setStroke(new BasicStroke(4));
         prevY = endY - (yCoords[0] * unitY);
         for (int y : yCoords) {
             if (firstElement) {
                 firstElement = false;
             } else {
-                g2d.drawLine(prevX, prevY, prevX += unitX, prevY = endY - (y * unitY));
+                g2d.drawLine(prevX, prevY, prevX += unitX, prevY = endY - (y * unitY * numHoritzontalLines / topElementY));
             }
         }
 
         //Drowing the axis of our graph
-        g2d.setColor(Color.BLACK);
-        g2d.drawString(Integer.toString(topElementY), startX - 20, startY + 5);
-        g2d.drawString("0", startX - 20, endY + 5);
-
-
-        g2d.drawString("Graph TITLE", endX / 2 + 50, endY + 105);
-        g2d.setColor(Color.RED);
+         g2d.setColor(Color.BLACK);
+        g2d.drawString(name, endX / 2 + 50, endY + 105);
+        g2d.setColor(lineColor);
         g2d.drawLine(endX / 2 - 50, endY + 100, endX / 2 + 40, endY + 100);
 
 
@@ -93,14 +94,14 @@ class GraphDrawer extends JPanel implements ComponentListener {
         g2d.setStroke(new BasicStroke(2));
         g2d.drawLine(startX, startY, startX, endY);
         g2d.drawLine(startX, endY, endX, endY);
-        this.componentResized(new ComponentEvent(this, ComponentEvent.COMPONENT_RESIZED));
     }
 
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(endX + 100, endY + 100);
+    private int numSections(int x) {
+        while (x > 10) {
+            x /= 10;
+        }
+        return x;
     }
-
     private int GetMax(int[] inputArray) {
         int maxValue = inputArray[0];
         for (int i = 1; i < inputArray.length; i++) {
@@ -110,26 +111,4 @@ class GraphDrawer extends JPanel implements ComponentListener {
         }
         return maxValue;
     }
-
-    public void componentResized(ComponentEvent e) {
-        endX = e.getComponent().getWidth() - 50;
-        endY = e.getComponent().getHeight() - 160;
-        unitX = (endX - startX) / 23;
-        prevX = startX;
-        prevY = endY;
-
-    }
-
-    @Override
-    public void componentMoved(ComponentEvent e) {
-    }
-
-    @Override
-    public void componentShown(ComponentEvent e) {
-    }
-
-    @Override
-    public void componentHidden(ComponentEvent e) {
-    }
-
 }
--- src/view/Ranking.java
+++ src/view/Ranking.java
@@ -15,12 +15,12 @@ public class Ranking extends JPanel {
     public Ranking(RankingEvent rankingE) {
 
         this.event = rankingE;
-        songsGraph = new GraphDrawer(this.event.getSongsStadistics(), "Listened Songs per hour", new Color(0xFF0000));
-        timeGraph = new GraphDrawer(this.event.getTimeStadistics(), "Listened Time per hour", new Color(0x016618));
+        songsGraph = new GraphDrawer(this.event.getSongsStadistics(), "Number of songs listened per hour", new Color(0xFF0000));
+        timeGraph = new GraphDrawer(this.event.getTimeStadistics(), "Number of seconds listened per hour", new Color(0x016618));
 
-        this.setLayout(new BorderLayout(10, 10));
-        this.add(songsGraph, BorderLayout.WEST);
-        this.add(timeGraph, BorderLayout.EAST);
+        this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+        this.add(songsGraph);
+        this.add(timeGraph);
         this.setBackground(ColorConstants.BACKGROUND.getColor());
         this.setVisible(true);
 
