4ea8080430265e8343519a1429ebae84e6819258
Carles Roch Arnau <carles.roch@students.salle.url.edu>
2021-05-20 17:39:50 +0200

    Piano controller

--- src/entities/Song.java
+++ src/entities/Song.java
@@ -11,7 +11,7 @@ public class Song {
      * Temps [us] per tick
      */
     private double tickLength;
-    private final String songName;
+    private String songName;
     private final String author;
     private final Date creationDate;
     private boolean isPublic;
@@ -88,6 +88,10 @@ public class Song {
 
     public boolean getPublic() {
         return this.isPublic;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
     }
 
     public void setTickLength(double tickLength) {
