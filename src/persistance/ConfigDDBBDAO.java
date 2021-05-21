43fe981c52d1c8fb162925eeb688d25c7dd9d2f9
Crochar6 <roch.carles@gmail.com>
2021-05-21 12:23:23 +0200

    setConfig

--- src/persistance/ConfigDDBBDAO.java
+++ src/persistance/ConfigDDBBDAO.java
@@ -1,6 +1,7 @@
 package persistance;
 
 import entities.Config;
+import entities.Note;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -51,5 +52,16 @@ public class ConfigDDBBDAO implements ConfigDAO{
         }
     }
 
-
+    @Override
+    public boolean setConfig(String nick, char[] characters) {
+        try {
+            for (int i = 0; i < 12 * Config.NUM_OCTAVES; i++) {
+                if (this.ddbb.runSentence("UPDATE PianoKeys SET keyboard = ? WHERE (user = (SELECT u.id FROM Users AS u JOIN RegisteredUsers AS ru ON u.id = ru.id WHERE u.username = ?) AND octave = ? AND note = ?);",
+                        String.valueOf(characters[i]), nick, i/12 + 1, Note.getNote(i).name().replaceAll("X$", "#")) == 0) return false;
+            }
+            return true;
+        } catch (SQLException ex) {
+            return false;
+        }
+    }
 }
