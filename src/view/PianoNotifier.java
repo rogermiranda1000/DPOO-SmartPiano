7ff6d171b4d8242a8579f3511537dfdb2b44439e
Crochar6 <roch.carles@gmail.com>
2021-05-22 21:22:52 +0200

    View comments (final)

--- src/view/PianoNotifier.java
+++ src/view/PianoNotifier.java
@@ -2,7 +2,19 @@ package view;
 
 import entities.SongNote;
 
+/**
+ * Manages actions regarding the piano keys
+ */
 public interface PianoNotifier {
+
+    /**
+     * Un-presses all the piano keys
+     */
     void unpressAllKeys();
+
+    /**
+     * A note should be pressed in the piano
+     * @param key Note to play
+     */
     void pressKey(SongNote key);
 }
--- src/view/UneditableTable.java
+++ src/view/UneditableTable.java
@@ -2,15 +2,33 @@ package view;
 
 import javax.swing.table.DefaultTableModel;
 
+/**
+ * Custom default table model that can't be edited by the user
+ */
 public class UneditableTable extends DefaultTableModel {
+
+    /**
+     * Calls the DefaultTableModel's constructor
+     */
     public UneditableTable() {
         super();
     }
 
+    /**
+     * Creates an uneditable table with the data
+     * @param tableData Data to show on the table
+     * @param columnID The names of the columns
+     */
     public UneditableTable(String[][] tableData, String[] columnID) {
         super(tableData, columnID);
     }
 
+    /**
+     * Returns if this cell is editable, it will always return false for this class
+     * @param row Row the user is trying to edit
+     * @param cols Column the user is trying to edit
+     * @return It wil always return false
+     */
     @Override
     public boolean isCellEditable(int row, int cols) {
         return false;
