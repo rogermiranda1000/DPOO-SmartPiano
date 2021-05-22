28b611631f336252cc267c552290aa98322fd2b8
roger.miranda <roger.miranda@students.salle.url.edu>
2021-05-22 15:22:02 +0200

    Re-pintar ranking cada 2 minuts

--- src/controller/RankingHourUpdater.java
+++ src/controller/RankingHourUpdater.java
@@ -4,13 +4,18 @@ import model.WebScrapping;
 import view.NewPlayNotifier;
 
 /**
- * Notifies the graph when an hour changes
+ * Notifies the graph each SECONDS_PER_UPDATE seconds (to update the hour and the external users reproductions)
  */
 public class RankingHourUpdater extends Thread {
     /**
+     * Time to notify the view that update is required
+     */
+    private static final int SECONDS_PER_UPDATE = 120;
+
+    /**
      * Connection with the view
      */
-    private NewPlayNotifier notifier;
+    private final NewPlayNotifier notifier;
 
     public RankingHourUpdater(NewPlayNotifier notifier) {
         this.notifier = notifier;
@@ -24,8 +29,8 @@ public class RankingHourUpdater extends Thread {
     public void run() {
         try {
             while (true) {
+                Thread.sleep( RankingHourUpdater.SECONDS_PER_UPDATE * 1000);
                 notifier.reloadGraphs();
-                Thread.sleep(1000);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
