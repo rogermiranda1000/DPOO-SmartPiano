dbaa4a78fc850c6763495e9bb9532cabf86424be
Carles Roch Arnau <carles.roch@students.salle.url.edu>
2021-05-23 15:47:42 +0200

    SongAdder comments

--- src/SongAdder.java
+++ src/SongAdder.java
@@ -9,7 +9,14 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.Date;
 
+/**
+ * Class that adds custom meme songs to the database
+ */
 public class SongAdder {
+
+    /**
+     * List with urls and song names of the meme songs to add
+     */
     private static final String[][] SONGS = {
             {"Megalovania", "http://www.vgmusic.com/new-files/UT-Megalovania.mid"},
             {"L'himne del Carbassot 100% oficial 1 sol link mega", "https://www.midiworld.com/download/5023"},
@@ -24,6 +31,11 @@ public class SongAdder {
             {"Crab rave", "https://joeybabcock.me/blog/wp-content/uploads/2019/02/Crab-Rave_4.mid"}
     };
 
+    /**
+     * Secondary entry point to add the custom meme songs
+     * @param args Arguments of the program (none used)
+     * @throws ClassNotFoundException The database driver is not installed
+     */
     public static void main (String[] args) throws ClassNotFoundException {
         try {
             Config c = new Config(new File("config.json"));
