cc26bfb47db99fb58a3b5a966a06f3b2369aa4d9
Crochar6 <roch.carles@gmail.com>
2021-05-23 12:40:17 +0200

    MenuEvent comments

--- src/controller/MenuEvent.java
+++ src/controller/MenuEvent.java
@@ -1,5 +1,12 @@
 package controller;
 
+/**
+ * Interface tasked with session exiting events
+ */
 public interface MenuEvent {
+
+    /**
+     * Makes the user log out and closes the session
+     */
     void exitSession();
 }
