edb0fb6d5725c878335b07f82ee432fa269b25b5
Carles Roch Arnau <carles.roch@students.salle.url.edu>
2021-05-23 13:06:25 +0200

    RecordingEvent comments

--- src/controller/RecordingEvent.java
+++ src/controller/RecordingEvent.java
@@ -1,32 +1,36 @@
 package controller;
 
+/**
+ * Interface tasked with actions regarding the recording of songs and muting the song the user is trying to play on the piano
+ */
 public interface RecordingEvent {
+
     /**
-     * Notify to the controller that the user start recording
-     * @param recording True if the user start recording, false if the user stops (save the recording)
+     * Notifies the controller that the user started recording
+     * @param recording True if the user started recording, false if the user stopped
      */
     void startRecording(boolean recording);
 
     /**
-     * Notify the controller that the recorded song must be saved
+     * Notifies the controller that the recorded song must be saved
      * @param name Song's name
-     * @param isPublic Song's visibility
+     * @param isPublic Song's visibility (true = public, false = private)
      */
     void saveRecordedSong(String name, boolean isPublic);
 
     /**
-     * Mute the octave INIT_OCTAVA to (INIT_OCTAVA+NUM_OCTAVES)
+     * Mute the octave INIT_OCTAVE to (INIT_OCTAVE + NUM_OCTAVES)
      */
     void muteSong();
 
     /**
-     * Unmute the octave INIT_OCTAVA to (INIT_OCTAVA+NUM_OCTAVES)
+     * Unmute the octave INIT_OCTAVE to (INIT_OCTAVE + NUM_OCTAVES)
      */
     void unmuteSong();
 
     /**
-     * Tells the Controller the user has stopped playing the piano.
-     * If the song is valid, the Controller should notify the view thought 'requestSongInformation()'.
+     * Tells the Controller the user has stopped recording the piano song,
+     * if the song is valid the Controller should notify the view thorough 'requestSongInformation()'.
      */
     void validateRecording();
 }
