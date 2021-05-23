55213b1b3b95cb2e6b70cc6785951dd28288b433
Crochar6 <roch.carles@gmail.com>
2021-05-23 15:27:44 +0200

    ColorConstants comments extra

--- src/view/ColorConstants.java
+++ src/view/ColorConstants.java
@@ -6,12 +6,40 @@ import java.awt.*;
  * Defines color constants commonly used
  */
 public enum ColorConstants {
+
+    /**
+     * Color used for the background of the app
+     */
     BACKGROUND(188, 205, 225),
+
+    /**
+     * Clor used for background menus
+     */
     MENU(135, 189, 189),
+
+    /**
+     * Color used for some buttons
+     */
     BUTTON(93, 130, 130),
+
+    /**
+     * Color used for the font of the top buttons when they aren't selected
+     */
     TOP_BUTTON_FONT(230, 241, 255),
+
+    /**
+     * Color used for the font of the top buttons when they are selected
+     */
     ACTIVE_BUTTON(106, 250, 188),
+
+    /**
+     * Color used for the "delete account" button
+     */
     RED_BUTTON(99, 20, 20),
+
+    /**
+     * Color used for the "confirm configuration" button
+     */
     GREEN_BUTTON(60, 163, 117);
 
     /**
