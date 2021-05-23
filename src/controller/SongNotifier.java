81d4447cee810d22f8e95081f3548d65476d8a91
Crochar6 <roch.carles@gmail.com>
2021-05-23 13:06:42 +0200

    SongNotifier comments

--- src/controller/SongNotifier.java
+++ src/controller/SongNotifier.java
@@ -2,6 +2,14 @@ package controller;
 
 import entities.Song;
 
+/**
+ * Interface tasked with informing when a song should be added to the database
+ */
 public interface SongNotifier {
+
+    /**
+     * Adds a song to the database
+     * @param song Song to add
+     */
     void addSong(Song song);
 }
