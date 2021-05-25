3333169503e928e35e3b75d19677d6a8784f22e9
roger.miranda <roger.miranda@students.salle.url.edu>
2021-05-25 13:38:37 +0200

    Hotfix: al entrar amb la base de dades buida no peta

--- src/view/GraphDrawer.java
+++ src/view/GraphDrawer.java
@@ -73,7 +73,9 @@ class GraphDrawer extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
 
         int topElementY = this.maxValue(yCoords);
+        if (topElementY == 0) topElementY = 1;
         int numHoritzontalLines = this.numSections(topElementY);
+        if (numHoritzontalLines == 0) numHoritzontalLines = 1;
 
         int hour = yCoords[yCoords.length-1];
         int endX = GraphDrawer.GRAPH_WIDTH;
